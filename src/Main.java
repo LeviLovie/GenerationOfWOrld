b7e6794a2d496f899340c5537fab5c1235f359e0
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2022-11-19 15:18:31 +0700

    Add window
    add app window

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+    private JFrame frame;
+    public Main() {
+        frame = new JFrame();
+        frame.setTitle("Generating of World");
+        frame.setSize(1280, 720);
+        frame.setLocation(0, 0);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("App started");
+        new Main();
     }
 }
