11722e67b7d0bf5ec277aee6fffb82f65ebb12f9
leviiloviee <107021730+LeviiLovie@users.noreply.github.com>
2022-11-20 01:08:46 +0700

    a

--- src/Window.java
+++ src/Window.java
@@ -71,7 +71,7 @@ public class Window extends JPanel {
 
         add(preferences, null);
     }
-    private Color[][] DayNightLandWater() {
+    private Color[][] dayNightLandWater() {
         System.out.println(GetTime() + ": Start - DayNightLandWater()");
         Color[][] localWorldArrayColor = this.world.GetWorldArray();
         int[][] localWorldArray = new int[localWorldArrayColor.length][localWorldArrayColor[0].length];
@@ -153,113 +153,34 @@ public class Window extends JPanel {
 
         return localWorldArrayColor;
     }
-    private Color[][] PerlinNoiseLandWater() {
-        System.out.println(GetTime() + ": Start - PerlinNoiseLandWater()");
+    public int[][] createWorld(int width, int height) {
+        return generateOctavedSimplexNoise(width, height);
+    }
 
-        BilinearInterpolation bilinearInterpolation = new BilinearInterpolation();
+    private int[][] generateOctavedSimplexNoise(int width, int height) {
+        double[][] totalNoise = new double[width][height];
+        double layerFrequency = 1;
+        double layerWeight = 1;
+        double weightSum = 0;
 
-        Color[][] localWorldArrayColor = this.world.GetWorldArray();
-        int[][] localWorldArray = new int[localWorldArrayColor.length][localWorldArrayColor[0].length];
+        // Summing up all octaves, the whole expression makes up a weighted average
+        // computation where the noise with the lowest frequencies have the least effect
 
-        BufferedImage noise10 = new BufferedImage(
-                localWorldArray[0].length, localWorldArray.length, BufferedImage.TYPE_INT_RGB
-        );
-        BufferedImage noise5 = new BufferedImage(
-                localWorldArray[0].length, localWorldArray.length, BufferedImage.TYPE_INT_RGB
-        );
-        BufferedImage noise2 = new BufferedImage(
-                localWorldArray[0].length, localWorldArray.length, BufferedImage.TYPE_INT_RGB
-        );
-        BufferedImage noise1 = new BufferedImage(
-                localWorldArray[0].length, localWorldArray.length, BufferedImage.TYPE_INT_RGB
-        );
-        BufferedImage noiseResult = new BufferedImage(
-                localWorldArray[0].length, localWorldArray.length, BufferedImage.TYPE_INT_RGB
-        );
-
-        int size;
-        size = 10;
-        for (int y = 0; y < Math.floor(localWorldArray.length / size); y++) {
-            for (int x = 0; x < Math.floor(localWorldArray[y].length / size); x++) {
-                for (int y10 = 0; y10 < Math.floor(localWorldArray.length / size); y10++) {
-                    for (int x10 = 0; x10 < Math.floor(localWorldArray[y].length / size); x10++) {
-                        int rand = this.world.rand(0, 255);
-                        try {
-                            noise10.setRGB(x * size + x10, y * size + y10, rand);
-                        } catch (Exception ignored) {}
-                    }
+        for (int octave = 0; octave < 2; octave++) {
+            // Calculate single layer/octave of simplex noise, then add it to total noise
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    totalNoise[x][y] += SimplexNoise.noise(x * layerFrequency, y * layerFrequency) * layerWeight;
                 }
             }
+
+            // Increase variables with each incrementing octave
+            layerFrequency *= 2;
+            weightSum += layerWeight;
+            layerWeight *= ROUGHNESS;
+
         }
-        size = 5;
-        for (int y = 0; y < Math.floor(localWorldArray.length / size); y++) {
-            for (int x = 0; x < Math.floor(localWorldArray[y].length / size); x++) {
-                for (int y5 = 0; y5 < Math.floor(localWorldArray.length / size); y5++) {
-                    for (int x5 = 0; x5 < Math.floor(localWorldArray[y].length / size); x5++) {
-                        int rand = this.world.rand(0, 255);
-                        try {
-                            noise5.setRGB(x * size + x5, y * size + y5, rand);
-                        } catch (Exception ignored) {}
-                    }
-                }
-            }
-        }
-//        size = 2;
-//        for (int y = 0; y < Math.floor(localWorldArray.length / size); y++) {
-//            for (int x = 0; x < Math.floor(localWorldArray[y].length / size); x++) {
-//                for (int y2 = 0; y2 < Math.floor(localWorldArray.length / size); y2++) {
-//                    for (int x2 = 0; x2 < Math.floor(localWorldArray[y].length / size); x2++) {
-//                        int rand = this.world.rand(0, 255);
-//                        try {
-//                            noise2.setRGB(x * size + x2, y * size + y2, rand);
-//                        } catch (Exception ignored) {}
-//                    }
-//                }
-//            }
-//        }
-//        size = 1;
-//        for (int y = 0; y < Math.floor(localWorldArray.length / size); y++) {
-//            for (int x = 0; x < Math.floor(localWorldArray[y].length / size); x++) {
-//                for (int y1 = 0; y1 < Math.floor(localWorldArray.length / size); y1++) {
-//                    for (int x1 = 0; x1 < Math.floor(localWorldArray[y].length / size); x1++) {
-//                        int rand = this.world.rand(0, 255);
-//                        try {
-//                            noise1.setRGB(x * size + x1, y * size + y1, rand);
-//                        } catch (Exception ignored) {}
-//                    }
-//                }
-//            }
-//        }
-
-//        noise10 = bilinearInterpolation.Interpolation(noise10);
-//        noise5 = bilinearInterpolation.Interpolation(noise5);
-//        noise2 = bilinearInterpolation.Interpolation(noise2);
-//        noise1 = bilinearInterpolation.Interpolation(noise1);
-
-        for (int y = 0; y < noiseResult.getHeight(); y++) {
-            for (int x = 0; x < noiseResult.getWidth(); x++) {
-                int noise10Color = noise10.getRGB(x, y) / 4;
-                int noise5Color = noise5.getRGB(x, y) / 4;
-//                int noise2Color = noise2.getRGB(x, y) / 4;
-//                int noise1Color = noise1.getRGB(x, y) / 4;
-
-//                noiseResult.setRGB(x, y, (noise10Color + noise5Color + noise2Color + noise1Color));
-                noiseResult.setRGB(x, y, (noise10Color + noise5Color));
-            }
-        }
-
-        for (int y = 0; y < localWorldArray.length; y++) {
-            for (int x = 0; x < localWorldArray[y].length; x++) {
-//                if (noiseResult.getRGB(x, y) > 0.5) {
-//                    localWorldArrayColor[y][x] = new Color(139, 195, 74);
-//                } else if (noiseResult.getRGB(x, y) < 0.5) {
-//                    localWorldArrayColor[y][x] = new Color(33, 150, 243);
-//                }
-                localWorldArrayColor[y][x] = new Color(noiseResult.getRGB(x,y));
-            }
-        }
-
-        return localWorldArrayColor;
+        return totalNoise;
     }
     private void calculateValuesForScreen() {
         System.out.println(GetTime() + ": Start - calculateValuesForScreen()");
@@ -343,52 +264,5 @@ class World {
     }
     private static String GetTime() {
         return String.valueOf(LocalDateTime.now());
-    }
-}
-
-class BilinearInterpolation {
-    /* gets the 'n'th byte of a 4-byte integer */
-    private static int get(int self, int n) {
-        return (self >> (n * 8)) & 0xFF;
-    }
-
-    private static float lerp(float s, float e, float t) {
-        return s + (e - s) * t;
-    }
-
-    private static float blerp(final Float c00, float c10, float c01, float c11, float tx, float ty) {
-        return lerp(lerp(c00, c10, tx), lerp(c01, c11, tx), ty);
-    }
-
-    private static BufferedImage scale(BufferedImage self, float scaleX, float scaleY) {
-        int newWidth = (int) (self.getWidth() * scaleX);
-        int newHeight = (int) (self.getHeight() * scaleY);
-        BufferedImage newImage = new BufferedImage(newWidth, newHeight, self.getType());
-        for (int x = 0; x < newWidth; ++x) {
-            for (int y = 0; y < newHeight; ++y) {
-                float gx = ((float) x) / newWidth * (self.getWidth() - 1);
-                float gy = ((float) y) / newHeight * (self.getHeight() - 1);
-                int gxi = (int) gx;
-                int gyi = (int) gy;
-                int rgb = 0;
-                int c00 = self.getRGB(gxi, gyi);
-                int c10 = self.getRGB(gxi + 1, gyi);
-                int c01 = self.getRGB(gxi, gyi + 1);
-                int c11 = self.getRGB(gxi + 1, gyi + 1);
-                for (int i = 0; i <= 2; ++i) {
-                    float b00 = get(c00, i);
-                    float b10 = get(c10, i);
-                    float b01 = get(c01, i);
-                    float b11 = get(c11, i);
-                    int ble = ((int) blerp(b00, b10, b01, b11, gx - gxi, gy - gyi)) << (8 * i);
-                    rgb = rgb | ble;
-                }
-                newImage.setRGB(x, y, rgb);
-            }
-        }
-        return newImage;
-    }
-    public BufferedImage Interpolation(BufferedImage source) {
-        return scale(source, 1.0f, 1.0f);
     }
 }
